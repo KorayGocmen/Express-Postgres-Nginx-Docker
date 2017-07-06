bc6b0b731bd175e7732d06812c9d2dbb67e48c08
koraygocmen <gocmen.koray@gmail.com>
2017-07-06 15:28:16 -0400

    remove apidoc in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Remove API Docs
+rm -rf apidoc
+
 # Installing docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
